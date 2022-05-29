1aa9744ccd637ff05dcc91cad1aa606386cdd673
Driss Alaoui <sulimani.driss.alaui@nure.ua>
2022-05-29 02:32:54 +0100

    --Good--work !!

--- Knowledge-Base.py
+++ Knowledge-Base.py
@@ -1,9 +1,11 @@
 from tkinter import *
 from tkinter import filedialog
 from tkinter import ttk
+from tkinter import colorchooser
 import PyPDF2
 from os import listdir
 from os.path import isfile, join
+from PIL import ImageTk,Image
 from tkPDFViewer import tkPDFViewer as pdf
 import os
 
@@ -15,7 +17,7 @@ root.iconbitmap("icon.ico")
 root.geometry("1500x850")
 
 
-
+########################################################################################################################
 main_frame = Frame(root)
 main_frame.pack(fill=BOTH, expand=1)
 
@@ -41,53 +43,155 @@ first_frame = Frame(second_frame)
 first_frame.grid(row=0,column=0,padx=(10,30))
 
 ########################################################################################################################
+global Size_Font
+global my_fg
+global theme
+global color_changed
+color_changed =False
+
+
+def color():
+    global my_fg
+    global color_changed
+    my_fg = colorchooser.askcolor()
+    color_changed = True
+
+
+
+
+def Font():
+    global Size_Font
+    global theme
+    top = Toplevel()
+    top.title("Font")
+    top.iconbitmap("font.ico")
+
+    Theme_list = ["Helvetica", "Terminal", "System", "Roman", "Adobe Arabic", "Narkisim"]
+    theme = StringVar()
+    theme.set("Theme Fonts")
+    theme_drop = OptionMenu(top, theme, *Theme_list)
+    theme_drop.grid(row=0,column=0,padx=(30,30))
+
+
+    Size_Font =  Scale(top, from_=8, to=72, orient=HORIZONTAL)
+    Size_Font.grid(row=0,column=1, padx=(30,30))
+
+    color_btn = Button(top,text="Pick a Font Color",command=color)
+    color_btn.grid(row=0, column=2, padx=(30,30),pady=20)
+
+    edit_btn = Button(top,text=" Edit Font ",command=get_Font)
+    edit_btn.grid(row=1, column=0,columnspan=3,ipadx=150, padx=(20, 30), pady=(40,30))
+
+
+def get_Font():
+    global color_changed
+    global Size
+    global my_fg
+    global theme
+    Size = str(Size_Font.get())
+
+    if color_changed:
+        my_text.config(font=(theme.get(), Size), fg=my_fg[1])
+    else:
+        my_text.config(font=(theme.get(), Size), fg=my_fg)
+
+
+
+
+
+
+#\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+global my_img
+global my_label
+global isExist
+global v2
+v2 = Label()
+isExist=False
+my_img = ImageTk.PhotoImage(Image.open("D:/Studies/img2.jpg"))
+
+my_label = Label(my_frame,image=my_img)
+
+
+
+########################################################################################################################
 def get_extn(filename):
     return filename[filename.rfind('.') + 1:]
 
-
 def open_Directory(value):
+    global isExist
+    global Read_btn
+    global my_label
+    global v2
     extn = get_extn(value)
     if extn == "txt":
+
+        my_scrollbar.config(command=my_text.yview)
+        my_scrollbar.pack(side=RIGHT, fill=Y)
+        my_text.pack(pady=20,ipadx=300)
+        v2.grid_forget()
+        my_label.grid_forget()
         text_file = open(value, 'r', encoding='cp850', errors='ignore')
         stuff = text_file.read()
         my_text.delete("1.0", END)
         my_text.insert(END, stuff)
         text_file.close()
+        Read_btn.grid_forget()
+        Read_btn.grid(row=1, column=2, pady=20, ipadx=150)
 
     elif extn == "pdf":
         if value:
 
-            my_text.delete("1.0", END)
-            pdf_file = PyPDF2.PdfFileReader(value)
-            page = pdf_file.getPage(1)
-            page_content = page.extract_text()
-            my_text.insert(1.0, page_content)
+            my_text.pack_forget()
+            v1=pdf.ShowPdf()
+            v2=v1.pdf_view(second_frame,pdf_location=open(value,"r"),width=77,height=100)
+            v2.grid(row=0, column=1, pady=(0,0))
+            Read_btn.grid(row=0, column=2, pady=20, ipadx=150)
+
+    elif extn == "jpg":
+            if value:
+
+                global my_img
+                v2.grid_forget()
+                my_img = ImageTk.PhotoImage(Image.open(value))
+                my_label = Label(second_frame,image=my_img)
+                my_scrollbar.pack_forget()
+                my_text.pack_forget()
+
+                my_label.grid(row=0,column=1)
+                Read_btn.grid_forget()
+                if isExist:
+                    Read_btn.grid(row=1, column=1, pady=20, ipadx=150)
 
     else:
         pass
 
+########################################################################################################################
 
 def Select_directory():
+    global isExist
     global first_frame
-    first_frame.destroy()
-
+    global Read_btn
     mypath = filedialog.askdirectory(initialdir="D:/Studies", title="Get Directory")
-    first_frame = LabelFrame(second_frame, text=mypath, fg="BLUE")
-    first_frame.grid(row=0, column=0, padx=(10, 30))
+
     if mypath:
+        first_frame.destroy()
+        first_frame = LabelFrame(second_frame, text=mypath, fg="BLUE")
+        first_frame.grid(row=0, column=0, padx=(10, 30))
+        my_text.delete("1.0", END)
         onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
         for index in range(0, len(onlyfiles) ):
-            my_text.insert(END, mypath+"/"+onlyfiles[index]+"\n")
             Radiobutton(first_frame, text=onlyfiles[index], variable=File, value=mypath+"/"+onlyfiles[index]).pack(anchor=SW)
-        if File:
-            open_Directory(File.get())
-        Read_btn = Button(second_frame, text="Read File", command=lambda:open_Directory(File.get()))
-        Read_btn.grid(row=1 , column=3,pady=20)
+        my_text.insert(END," Select a File ....\n")
+        Read_btn = Button(second_frame, text="Open File", command=lambda:open_Directory(File.get()))
+        if not isExist:
+            Read_btn.grid(row=1 , column=3,pady=20, ipadx=150)
+            isExist = True
         #open_Directory_btn.config(state=DISABLED)
     else:
         pass
 
 
+########################################################################################################################
 
 def open_file():
 
@@ -110,8 +214,9 @@ def open_file():
     else:
         pass
 
+########################################################################################################################
 
-def save_txt():
+def save_file():
     text_file = filedialog.asksaveasfilename(initialdir="D:/Studies", title="Save Text File",filetypes=(("Text File","*.txt"),("All","*.*"),))
     if text_file:
         text_file = open(text_file,'w')
@@ -122,35 +227,41 @@ def save_txt():
         pass
 
 
-
+########################################################################################################################
 File = StringVar()
 File.set("*")
 
 
 
+Size = '16'
+my_fg = "Black"
+theme = "Adobe Arabic"
 
-
-
-my_text = Text(my_frame, width=40, height=20, font=("Helvetica", 16))
+my_text = Text(my_frame, width=40, height=20, font=(theme, Size),fg=my_fg)
 my_scrollbar.config(command= my_text.yview)
 my_scrollbar.pack(side=RIGHT, fill=Y)
 
-
-
 my_text.pack(pady=20,ipadx=300)
 
+########################################################################################################################
 
+my_menu = Menu(second_frame)
+root.config(menu=my_menu)
 
-open_btn = Button(second_frame, text="Open File", command=open_file)
-open_btn.grid(row=1 , column=0,pady=20,padx=(30,0))
+#Create a menu items
+file_menu = Menu(my_menu)
+my_menu.add_cascade(label="File",menu=file_menu)
+file_menu.add_command(label="Open Directory",command=Select_directory)
+file_menu.add_separator()
+file_menu.add_command(label="Open File",command=open_file)
+file_menu.add_command(label="New File",command=lambda:my_text.delete("1.0", END))
+file_menu.add_command(label="Save File",command=save_file)
 
-save_btn = Button(second_frame, text="Save File", command=save_txt)
-save_btn.grid(row=1 , column=1,pady=20)
+view_menu = Menu(my_menu)
+my_menu.add_cascade(label="View",menu=view_menu)
+view_menu.add_command(label="Font",command=Font)
 
-open_Directory_btn = Button(second_frame, text="Open Directory",command=Select_directory)
-open_Directory_btn.grid(row=1 , column=2,pady=20)
-
-
-
+exit_menu = Menu(my_menu)
+my_menu.add_command(label="Exit",command=quit)
 
 root.mainloop()
